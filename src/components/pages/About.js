5c71b62f292e76670bc18a9cc70b4c58952cdd7a
Stephen <stephensouness@outlook.com>
2023-03-08 12:46:12 +0000

    slight change to about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,44 +5,42 @@ function About() {
     <div className="about-box">
       <h1 className="about-title">About</h1>
       <p>
-        Welcome to Code Friends, the social networking site for developers! Our
-        mission is to connect developers from around the world and help them
-        build lasting relationships based on their shared interests and
-        experiences. Here's how to use the website:
+        Welcome to Code Friends, the social networking site designed exclusively
+        for developers! Our mission is to facilitate connections between
+        developers from around the globe, fostering enduring relationships based
+        on shared interests and experiences. Here's a quick guide on how to
+        utilize our website:
       </p>
       <p>
-        By clicking on the "Create Post" link in the navigation bar, you can
-        immediately jump in and create your own post, by providing a title,
-        image and description of your choice. The post will be marked with your
-        name and the date it was created. Once you confirm the details with the
-        submit button (you will receive a final confirmation message before
-        being returned to the home page), you will immediately be taken to the
-        home page where the new post will appear at the top of the screen.
+        To create a post, simply click on the "Create Post" link located in the
+        navigation bar. From there, you can customize your post by providing a
+        title, image, and description. Your name and the creation date will be
+        marked on the post. Once you hit the submit button, you will receive a
+        final confirmation message before being redirected to the homepage. Your
+        new post will appear at the top of the screen.
       </p>
       <p>
-        We have taken extra care to make the site as user-friendly as possible.
-        In the "Home" page you will be able view all the posts that have been
-        created. By clicking on a post, you will immediately be taken to a more
-        detailed version, where you can read the full description and comments,
-        as well as give the post a thumbs up if the content appeals to you.
+        We have made the website as user-friendly as possible. On the "Home"
+        page, you can browse all posts created by other users. By clicking on a
+        post, you can view the full description, comments, and give it a thumbs
+        up if you find the content appealing.
       </p>
       <p>
-        Other users will be able to comment on the post, which will be marked
-        with the name of the author and date of creation. The new comment will
-        appear at the top of the list.
+        Other users can comment on your post, which will be marked with their
+        name and the creation date. New comments will appear at the top of the
+        list.
       </p>
       <p>
-        You can easily return to the home page by clicking the cross at the top
-        right of the post title. If you are not a fan of the content you can
-        permanently delete it with the delete button (you will receive a warning
-        message in case you are not 100% sure). Comments can also be deleted in
-        the same way.
+        You can easily return to the homepage by clicking the "X" button located
+        at the top right of the post title. If you no longer want your post to
+        be available, you can delete it permanently with the delete button (be
+        aware that a warning message will pop up if you are not completely
+        sure). Comments can also be deleted in the same way.
       </p>
       <p>
-        While we do intend on implementing authentication elements to the
-        website, including the ability to log in and out, as well as provding a
-        separate section for your own profile, we hope you enjoy the site in its
-        current form!
+        While we are planning to add authentication features, such as the
+        ability to log in and out and a separate section for your profile, we
+        hope you enjoy our website in its current form!
       </p>
     </div>
   );
